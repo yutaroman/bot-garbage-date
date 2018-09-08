c5493acaf9affe9441dd040fb3efdee844178044
yutaro <yutaro.21.com@gmail.com>
2018-09-08 19:52:20 +0900

    revert

--- index.js
+++ index.js
@@ -1,48 +1,46 @@
-// -----------------------------------------------------------------------------
-// モジュールのインポート
-const server = require("express")();
-const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
+// modules import
+const server = require('express')();
+const line = require('@line/bot-sdk');
 
-// -----------------------------------------------------------------------------
-// パラメータ設定
+// パラメータの設定
 const line_config = {
-    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
-    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
-};
+    // 環境変数からアクセストークンをセット
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+    // 環境変数から Channel Secret をセット
+    channelSecret: process.env.LINE_CHANNEL_SECRET
+}
 
-// -----------------------------------------------------------------------------
-// Webサーバー設定
+// Web サーバーの設定
 server.listen(process.env.PORT || 3000);
 
-
-// APIコールのためのクライアントインスタンスを作成
+// API コールの為のクライアントインスタンスを作成
 const bot = new line.Client(line_config);
 
-// -----------------------------------------------------------------------------
-// ルーター設定
+// ルーターの設定
 server.post('/webhook', line.middleware(line_config), (req, res, next) => {
-    // 先行してLINE側にステータスコード200でレスポンスする。
+    // 先行して LINE 側にステータスコード 200 でレスポンスする。
     res.sendStatus(200);
 
-    // すべてのイベント処理のプロミスを格納する配列。
+    // Test code
+    // console.log(req.body);
+
+    // イベント処理のプロセスを格納する配列
     let events_processed = [];
 
-    // イベントオブジェクトを順次処理。
+    // イベントオブジェクトを順次処理
     req.body.events.map((event) => {
-        // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
-        if (event.type == "message" && event.message.type == "text"){
-            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-            if (event.message.text == "こんにちは"){
-                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+        if (event.type == 'message' && event.message.type == 'text') {
+            if (event.message.text == 'こんにちは') {
+                // replyMessage 関数で返信し、そのプロミスを events_processed に追加
                 events_processed.push(bot.replyMessage(event.replyToken, {
-                    type: "text",
-                    text: "これはこれは"
+                    type: 'text',
+                    text: 'これはこれは'
                 }));
             }
         }
     });
 
-    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
+    // 全てのイベント処理が終了したら、何個のイベントが処理されたかを出力
     Promise.all(events_processed).then(
         (response) => {
             console.log(`${response.length} event(s) processed.`);
