e4f2777874ceacbb72da98fadab950d81faf35a4
yutaro <yutaro.21.com@gmail.com>
2018-09-08 19:39:13 +0900

    test

--- index.js
+++ index.js
@@ -1,52 +1,23 @@
-// modules import
-const server = require('express')();
-const line = require('@line/bot-sdk');
+// -----------------------------------------------------------------------------
+// モジュールのインポート
+const server = require("express")();
+const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
 
-// パラメータの設定
+// -----------------------------------------------------------------------------
+// パラメータ設定
 const line_config = {
-    // 環境変数からアクセストークンをセット
-    channelAccessToken: process.env.LINE_ACCESS_TOKEN,
-    // 環境変数から Channel Secret をセット
-    channelSecret: process.env.LINE_CHANNEL_SECRET,
-}
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
+    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
+};
 
-// Web サーバーの設定
+// -----------------------------------------------------------------------------
+// Webサーバー設定
 server.listen(process.env.PORT || 3000);
 
-// API コールの為のクライアントインスタンスを作成
-const bot = new line.Client(line_config);
 
-// ルーターの設定
+// -----------------------------------------------------------------------------
+// ルーター設定
 server.post('/webhook', line.middleware(line_config), (req, res, next) => {
-    // 先行して LINE 側にステータスコード 200 でレスポンスする。
     res.sendStatus(200);
-
-    // Test code
-    // console.log(req.body);
-
-    // イベント処理のプロセスを格納する配列
-    let events_processed = [];
-
-    // イベントオブジェクトを順次処理
-    req.body.events.forEach((event) => {
-        if (event.type === 'message' && event.message.type === 'text') {
-            if (event.message.text === 'こんにちは') {
-                // replyMessage 関数で返信し、そのプロミスを events_processed に追加
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                    type: 'text',
-                    message: 'これはこれは'
-                }));
-            }
-        }
-    });
-
-    // 全てのイベント処理が終了したら、何個のイベントが処理されたかを出力
-    Promise.all(events_processed).then(
-        (response) => {
-            console.log(`${response.length} event(s) processed.`);
-        }
-    );
-
-    // debug
-    process.on('unhandledRejection', console.dir);
+    console.log(req.body);
 });
