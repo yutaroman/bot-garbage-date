46e0047ea03fab05b5f4e8db56bf122f9a8c4467
yutaro <yutaro.21.com@gmail.com>
2018-09-09 15:43:50 +0900

    debug

--- index.js
+++ index.js
@@ -47,6 +47,8 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
                         }
                     }
                 }).then((responses) => {
+                    console.dir(responses);
+
                     if (responses[0].queryResult && responses[0].queryResult.action === 'handle-garbage-question') {
                         let responseWeek = responses[0].queryResult.parameters.fields.date.stringValue;
                         let message_text = '';
